97c5fbe3c6df32d4bb774ae5e7c1585e90ea1216
mugambiDaktari <wycliffmutethia@gmail.com>
2025-03-22 16:39:27 +0300

    Linked Home page to menu, orders and inventoty API Endpoints

--- hotel_management_system/urls.py
+++ hotel_management_system/urls.py
@@ -1,23 +1,34 @@
-"""
-URL configuration for hotel_management_system project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from hotel_app.views import (
+    UserViewSet, MenuItemViewSet, OrderViewSet, OrderItemViewSet,
+    ReceiptViewSet, SalesReportViewSet, InventoryViewSet,
+    HomeView, RegisterView, UserLoginView, UserLogoutView
+)
+
+# DRF Router
+router = DefaultRouter()
+router.register(r'users', UserViewSet, basename='user') 
+router.register(r'menu-items', MenuItemViewSet, basename='menuitem')
+router.register(r'orders', OrderViewSet, basename='order')
+router.register(r'order-items', OrderItemViewSet, basename='orderitem')
+router.register(r'receipts', ReceiptViewSet, basename='receipt')
+router.register(r'sales-reports', SalesReportViewSet, basename='salesreport')
+router.register(r'inventory', InventoryViewSet, basename='inventory')
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('hotel_app.urls')),
+    
+    # Authentication & Home
+    path('', HomeView.as_view(), name='homepage'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('logout/', UserLogoutView.as_view(), name='logout'),
+
+    # DRF API Endpoints
+    path('api/', include(router.urls)),
 ]
+

4ec785b06694c153b23a683adac4a626bf1df4ca
mugambiDaktari <wycliffmutethia@gmail.com>
2025-04-03 11:59:46 +0300

    managed static files in settings.py

--- hotel_management_system/settings.py
+++ hotel_management_system/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -132,9 +133,10 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Ensure static files are collected
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "hotel_app", "static")]
+if DEBUG:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, "hotel_app", "static")]
 
-
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
